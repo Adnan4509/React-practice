c2dfa351e0e8a29c90dcfb6b7868116224ee6a81
Adnan4509 <adnanwebdev7@gmail.com>
2025-07-04 23:22:06 +0530

    Change state in class component

--- src/stateInClass.jsx
+++ src/stateInClass.jsx
@@ -6,10 +6,18 @@ export default class Exm extends Component {
         super(props);               //this is passing the prop to upper tree i.e., component class
 
         this.state = {
-            name: "",
-            age: 100,
+            name: "John",
+            age: 30,
             isMale: true,
         };
+    }
+
+    changeGender(){                 //Using onclick function to change state 
+        this.setState({
+            name: "Victoria",
+            age: "25",
+            isMale: false,
+        })
     }
     render(){
         const { name, age, isMale} = this.state;        // we used this const to avoid writing "this.state.name", "age" and "isMale" 
@@ -19,6 +27,7 @@ export default class Exm extends Component {
             <h1>My name is {name}</h1>
             <h2>I am {age} years old</h2>
             <h3>I am a {isMale ? "Male" : "Female"}</h3>
+            <button onClick={() => this.changeGender()}>No</button>
 
             <h4>I am feeling {this.props.emotion}</h4>              {/*"this" is pointing towards the instance of component which is emotion */}
         </div>
