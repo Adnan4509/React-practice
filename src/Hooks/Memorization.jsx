a8518d5f68de8596f1060666d3d643de23e3a08b
Adnan4509 <adnanwebdev7@gmail.com>
2025-07-06 15:25:14 +0530

    using use memo with dependency array

--- src/Hooks/Memorization.jsx
+++ src/Hooks/Memorization.jsx
@@ -11,13 +11,27 @@ function ExpensiveCount() {
   const [count, setCount] = useState(0);
   const [number, setNumber] = useState(nums);
 
-  const magical = number.find((item) => item.isMagical === true); //expensive computation
+//   const magical = number.find(item => item.isMagical===true) //expensive computation
+  const magical = useMemo(()=>number.find((item) => item.isMagical === true),[number])  //using useMemo to re-render it once or based on dependency
+  
+
 
   return (
     <div>
       <span><p>Magical number is: {magical.index}</p></span>
       <div>
-        <button onClick={()=> setCount((count) => count + 1)}>count is: {count}</button>
+    <button onClick={()=> {
+        setCount((count) => count + 1);
+        if(count == 10){
+            setNumber(new Array(10_000_000).fill(0).map((_, i) => {
+  return {
+    index: i,
+    isMagical: i === 9_00_000,
+  }
+}))
+        }
+        }}>count is: {count}
+        </button>
         <h1>hi</h1>
       </div>
     </div>
