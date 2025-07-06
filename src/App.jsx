1178f9076e4d139adce1e203be065f4e6dd7d7f8
Adnan4509 <adnanwebdev7@gmail.com>
2025-07-06 15:06:01 +0530

    expensive computation without useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Exm from './stateInClass'
 import Exm1 from './stateInFunction'
 import './App.css'
+import ExpensiveCount from './Hooks/Memorization'
 
 function App() {
 
   return (
     <>
-     <Exm emotion="Happy"/> 
-     <Exm1 emotion="Energetic"/>
+     {/* <Exm emotion="Happy"/> 
+     <Exm1 emotion="Energetic"/> */}
+     <ExpensiveCount/>
     </>
   )
 }
---
+++ src/Hooks/Memorization.jsx
@@ -0,0 +1,26 @@
+import { useState, useMemo } from "react";
+
+const nums = new Array(50_000_000).fill(0).map((_, i) => {
+  return {
+    index: i,
+    isMagical: i === 49_00_000,
+  };
+});
+
+function ExpensiveCount() {
+  const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(nums);
+
+  const magical = number.find((item) => item.isMagical === true); //expensive computation
+
+  return (
+    <div>
+      <span><p>Magical number is: {magical.index}</p></span>
+      <div>
+        <button onClick={()=> setCount((count) => count + 1)}>count is: {count}</button>
+        <h1>hi</h1>
+      </div>
+    </div>
+  );
+}
+export default ExpensiveCount;
