8643142960031040e12a06ce9bd977085629ff58
Adnan4509 <adnanwebdev7@gmail.com>
2025-07-12 13:02:51 +0530

    changing variable without usestate and useref

---
+++ src/Hooks/useRefhook.jsx
@@ -0,0 +1,14 @@
+// import { useRef } from "react"
+
+const Reference = () => {
+    let a = 1
+    a = a + 1;
+
+    return(
+        <div>
+            <p>{a}</p>
+        </div>
+    )
+}
+
+export default Reference
