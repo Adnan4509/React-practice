0315d59af0242e2c284497a032ca49be83d7186c
Adnan4509 <adnanwebdev7@gmail.com>
2025-07-12 13:03:59 +0530

    changing variable without usestate and useref

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
 import './App.css'
-import { useState, useCallback} from 'react'
-import Navbar from './Components/Navbar'
+import { useState } from 'react'
+import Reference from './Hooks/useRefhook'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
-  // function getAdjective(){
-  //   return  "another" + count                 // whenever re-render happens this function changes
-  // }
-
-// using useCallback to freeze function
-  const getAdjective = useCallback(() =>{
-    return "another" + count                    // Here the function will re-render whenever count is changed
-  },[count])                                       
+                                
 
   return (
     <>
-     <Navbar adjective={"good"} getAdjective={getAdjective()}></Navbar>  {/*whenever count is changed this component is also rendered */}
     <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-
+        <Reference/>
       </div>
     </>
   )
